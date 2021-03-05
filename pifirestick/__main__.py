8856ec797095161de46e1e89b49effaf038c4451
xesued <nthnnrtn@gmail.com>
2021-03-05 17:55:45 -0600

    making headway

--- pifirestick/__main__.py
+++ pifirestick/__main__.py
@@ -1,5 +1,26 @@
+import argparse
+import json
 
-import ir.read
+p = argparse.ArgumentParser()
+g = p.add_mutually_exclusive_group(required=True)
+g.add_argument("-s", "--start", help="Start up the pi_firestick", action="store_true")
+g.add_argument(
+    "-r", "--record_ir", help="Start up the pi_firestick", action="store_true"
+)
 
+args = p.parse_args()
 
-print("Hi there")
+if args.start:
+    print("Time to play")
+    try:
+        f = open("remote_codes", "r")
+    except:
+        print("Can't open codes file")
+        exit(0)
+
+    codes = json.load(f)
+    print(codes)
+
+else:
+    print("Lets read in the values")
+    # import ir.read
