eb9217ebf02353df7699bb86e15693498f39580f
Nathan Norton <nathan.norton@leadpages.com>
2021-03-06 13:37:23 -0600

    Getting close

--- pifirestick/__main__.py
+++ pifirestick/__main__.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 
+from pifirestick.remote.arcade_stick import ArcadeStick
+
 p = argparse.ArgumentParser()
 g = p.add_mutually_exclusive_group(required=True)
 g.add_argument("-s", "--start", help="Start up the pi_firestick", action="store_true")
@@ -19,8 +21,17 @@ if args.start:
         exit(0)
 
     codes = json.load(f)
-    print(codes)
+    stick = ArcadeStick.get_arcade() 
+    try:
+        for key_input in stick.on_input(): 
+            print('--- ')
+            print(key_input)
+            # TODO: Find a method to make this work with edge GPIO detection,
+            # or some sort of change detection from the USB.
+            # time.sleep(0.1)
 
+    except KeyboardInterrupt:
+        sys.exit(0)
 else:
     print("Lets read in the values")
     # import ir.read
---
+++ pifirestick/remote/arcade_stick.py
@@ -0,0 +1,56 @@
+import evdev
+
+JS_UP = "UP"
+JS_DOWN = "DOWN"
+JS_LEFT = "LEFT"
+JS_RIGHT = "RIGHT"
+JS_X_CENTER = "X_CENTER"
+JS_Y_CENTER = "Y_CENTER"
+BTN_A = "A"
+BTN_B = "B"
+BTN_C = "C"
+BTN_D = "D"
+
+class ArcadeStick:
+  device = None
+
+  @staticmethod
+  def get_arcade():
+    """
+    Gets the remove device
+    """
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    # TODO: Better method to find the joystick?
+    d = next(device for device in devices if device.info.vendor == 121)
+
+    return ArcadeStick(d)
+  
+  def __init__(self, device):
+     self.device = device 
+
+  def read_input(self, cb):
+    """
+    Calls the function given when there is an input to the arcade stick.  
+    
+    """
+    for event in self.device.read_loop():
+      if event.type == evdev.ecodes.EV_ABS and event.code == evdev.ecodes.ABS_Z:
+        continue  # ignore z-axis
+
+      # X axis
+      if event.type == evdev.ecodes.EV_ABS and event.code == evdev.ecodes.ABS_X:
+        if event.value > 200:
+          yield JS_RIGHT
+        if event.value < 50:
+          yield JS_LEFT
+        else:
+          yield JS_X_CENTER
+
+      # Y axis
+      if event.type == evdev.ecodes.EV_ABS and event.code == evdev.ecodes.ABS_Y:
+        if event.value > 200:
+          yield JS_UP
+        if event.value < 50:
+          yield JS_DOWN
+        else:
+          JS_Y_CENTER
